077050b2f3298241b37ef268c9b262c73ad9c979
shitis <ssaxena.ece@gmail.com>
2014-08-18 15:18:15 +0530

    refactored code for UI

--- ui/app/scripts/controllers/workbench.js
+++ ui/app/scripts/controllers/workbench.js
@@ -4,14 +4,20 @@ angular.module('uiApp')
     .controller('WorkbenchCtrl', ['$rootScope', '$scope', '$location', '$timeout', 'WebSocketService', function ($rootScope, $scope, $location, $timeout, WebSocket) {
         'use strict';
 
-        var queryId = 1;
+        var queryId = 1,
+            isQueryForLeftPanel = false;
 
         $scope.tabs = [
-            { id: 1, title: "Query 1", query: "", result: [], active: true},
-            { id: 2, title: "Query 2", query: "", result: [] }
+            { id: 1, title: "Query 1", query: "", result: [], active: true, error: ""},
+            { id: 2, title: "Query 2", query: "", result: [], error: ""}
         ];
 
         $scope.queryTab = [];
+
+        $scope.fetchKeyspaceData = function () {
+            isQueryForLeftPanel = true;
+            WebSocket.query("select * from system.schema_keyspaces", 0);
+        };
 
         function updateKeyspaces(data) {
             var keyspaces = [];
@@ -23,37 +29,41 @@ angular.module('uiApp')
             return keyspaces;
         }
 
-        function queryResultHandler(data) {
-            var expectedKeys = ["keyspace_name", "durable_writes", "strategy_class", "strategy_options"],
-                dataKeys = [],
-                tabId,
-                resultTab;
-            if (data.status === "QueryResult") {
-                dataKeys = Object.keys(data.payload[0]);
-                if (_.difference(dataKeys, expectedKeys).length === 0) {
-                    $scope.keyspaces = updateKeyspaces(data.payload);
-                } else {
-                    tabId = $scope.queryTab.filter(function (t) {
-                        return t.queryId === data.queryId;
-                    })[0].id;
-                    resultTab = _.where($scope.tabs, {id: tabId})[0];
-                    if (resultTab) {
-                        resultTab.error = "";
-                        resultTab.result = data.payload;
-                        resultTab.resultCols = _.map(Object.keys(data.payload[0]), function (k) {
-                            return {field: k, displayName: k}
-                        });
-                    }
-                }
-            } else if (data.status === "Invalid Query") {
+        function getResultTab(data) {
+            var tabId,
+                resultTab = {query: "", result: [], error: ""};
+
+            if (!isQueryForLeftPanel && data.queryId !== undefined) {
                 tabId = $scope.queryTab.filter(function (t) {
                     return t.queryId === data.queryId;
                 })[0].id;
                 resultTab = _.where($scope.tabs, {id: tabId})[0];
-                if (resultTab) {
+            }
+            return resultTab
+        }
+
+        function getColumnDef(columns) {
+            return _.map(columns, function (k) {
+                return {field: k, displayName: k}
+            });
+        }
+
+        function queryResultHandler(data) {
+            var resultTab = getResultTab(data);
+
+            switch (data.status) {
+                case "QueryResult":
+                    if (isQueryForLeftPanel) {
+                        $scope.keyspaces = updateKeyspaces(data.payload);
+                        isQueryForLeftPanel = false;
+                    } else if (data.payload.length > 0) { //do nothing when payload==0
+                        resultTab.result = data.payload;
+                        resultTab.resultCols = getColumnDef(Object.keys(data.payload[0]));
+                    }
+                    break;
+                case "InvalidQuery":
                     resultTab.error = data.errorMsg;
-                    resultTab.result = [];
-                }
+                    break;
             }
             $scope.$apply();
         }
@@ -72,7 +82,7 @@ angular.module('uiApp')
         }
 
         if ($rootScope.status === "Connected") {
-            WebSocket.query("select * from system.schema_keyspaces", 0);
+            $scope.fetchKeyspaceData();
         }
 
         $scope.disconnect = function () {
@@ -81,4 +91,13 @@ angular.module('uiApp')
 
         WebSocket.registerHandle(queryResultHandler);
         WebSocket.registerHandle(disconnectHandler);
+
+        $scope.isEmptyResponse = function (tab) {
+            return (!isQueryForLeftPanel && tab.result.length === 0);
+        };
+
+        $scope.isInvalidQuery = function (tab) {
+            return tab.error.length > 0;
+        };
+
     }]);
